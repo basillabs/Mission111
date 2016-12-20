e2885af642a99e80808d4bcdc0b497b969078c78
Brett Bukowski <brett.bukowski@gmail.com>
2016-12-19 20:59:09 -0800

    Add supported languages

---
+++ data/languages.js
@@ -0,0 +1,42 @@
+[
+  {
+    "name": "English",
+    "code": "EN",
+    "display_names": {
+      "EN": "English",
+      "SV": "Engelska",
+      "FA": "انگلیسی",
+      "AR": "الإنجليزية"
+    }
+  },
+  {
+    "name": "Swedish",
+    "code": "SV",
+    "display_names": {
+      "EN": "Swedish",
+      "SV": "Svenska",
+      "FA": "سوئد",
+      "AR": "اللغة السويدية"
+    }
+  },
+  {
+    "name": "Farsi",
+    "code": "FA",
+    "display_names": {
+      "EN": "Farsi",
+      "SV": "Persiska",
+      "FA": "فارسی",
+      "AR": "الفارسية"
+    }
+  },
+  {
+    "name": "Arabic",
+    "code": "AR",
+    "display_names": {
+      "EN": "Arabic",
+      "SV": "Arabiska",
+      "FA": "عربی",
+      "AR": "العربية"
+    }
+  }
+]
