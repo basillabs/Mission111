2db148b7bd5e6641b51fff8a9c8864c52beac18b
Brett Bukowski <brett.bukowski@gmail.com>
2016-12-19 21:29:54 -0800

    Add more structure

--- data/languages.js
+++ data/languages.js
@@ -1,42 +1,51 @@
-[
-  {
-    "name": "English",
-    "code": "EN",
-    "display_names": {
-      "EN": "English",
-      "SV": "Engelska",
-      "FA": "انگلیسی",
-      "AR": "الإنجليزية"
-    }
-  },
-  {
-    "name": "Swedish",
-    "code": "SV",
-    "display_names": {
-      "EN": "Swedish",
-      "SV": "Svenska",
-      "FA": "سوئد",
-      "AR": "اللغة السويدية"
-    }
-  },
-  {
-    "name": "Farsi",
-    "code": "FA",
-    "display_names": {
-      "EN": "Farsi",
-      "SV": "Persiska",
-      "FA": "فارسی",
-      "AR": "الفارسية"
-    }
-  },
-  {
-    "name": "Arabic",
-    "code": "AR",
-    "display_names": {
-      "EN": "Arabic",
-      "SV": "Arabiska",
-      "FA": "عربی",
-      "AR": "العربية"
+{
+  "supported_languages": [
+    "EN",
+    "SV",
+    "FA",
+    "AR"
+  ],
+  "languages": {
+    "EN": {
+      "name": "English",
+      "code": "EN",
+      "display_names": {
+        "EN": "English",
+        "SV": "Engelska",
+        "FA": "انگلیسی",
+        "AR": "الإنجليزية"
+      }
+    },
+    "SV": {
+      "name": "Swedish",
+      "code": "SV",
+      "display_names": {
+        "EN": "Swedish",
+        "SV": "Svenska",
+        "FA": "سوئد",
+        "AR": "اللغة السويدية"
+      }
+    },
+    "FA": {
+      "name": "Farsi",
+      "code": "FA",
+      "display_names": {
+        "EN": "Farsi",
+        "SV": "Persiska",
+        "FA": "فارسی",
+        "AR": "الفارسية"
+      }
+    },
+    "AR": {
+      "name": "Arabic",
+      "code": "AR",
+      "display_names": {
+        "EN": "Arabic",
+        "SV": "Arabiska",
+        "FA": "عربی",
+        "AR": "العربية"
+      }
     }
   }
-]
+}
+
