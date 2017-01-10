890ca52bde4a7f69a629789cac8033e691b24ecc
Brett Bukowski <brett.bukowski@gmail.com>
2017-01-09 19:57:46 -0800

    Add supported languages (#12)
    * Add supported languages
    
    * Add more structure

---
+++ data/languages.js
@@ -0,0 +1,51 @@
+{
+  "supported_languages": [
+    "EN",
+    "SV",
+    "FA",
+    "AR"
+  ],
+  "languages": {
+    "EN": {
+      "name": "English",
+      "code": "EN",
+      "display_names": {
+        "EN": "English",
+        "SV": "Engelska",
+        "FA": "انگلیسی",
+        "AR": "الإنجليزية"
+      }
+    },
+    "SV": {
+      "name": "Swedish",
+      "code": "SV",
+      "display_names": {
+        "EN": "Swedish",
+        "SV": "Svenska",
+        "FA": "سوئد",
+        "AR": "اللغة السويدية"
+      }
+    },
+    "FA": {
+      "name": "Farsi",
+      "code": "FA",
+      "display_names": {
+        "EN": "Farsi",
+        "SV": "Persiska",
+        "FA": "فارسی",
+        "AR": "الفارسية"
+      }
+    },
+    "AR": {
+      "name": "Arabic",
+      "code": "AR",
+      "display_names": {
+        "EN": "Arabic",
+        "SV": "Arabiska",
+        "FA": "عربی",
+        "AR": "العربية"
+      }
+    }
+  }
+}
+
