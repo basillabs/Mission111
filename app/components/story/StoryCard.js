9ec9fe3c7678009da595e835d7c7b9cd0c6dbc8c
Ako0 <amandako@gmail.com>
2017-02-13 20:29:29 -0800

    animating the view toggle (#27)
    * first pass view mode toggling
    
    * animate toggle view
    
    * putting midHeight calc into a const

--- app/components/story/StoryCard.js
+++ app/components/story/StoryCard.js
@@ -4,18 +4,50 @@ import {
   Text,
   StyleSheet,
   Dimensions,
+  Animated,
 } from 'react-native';
 
+const midHeight = Dimensions.get('window').height/2;
+
 class StoryCard extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      height: new Animated.Value() 
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.isSplit) {
+      this.state.height.setValue(midHeight);
+    } else {
+      this.state.height.setValue(0);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isSplit != this.props.isSplit) {
+      Animated.timing(
+        this.state.height,
+        { 
+          toValue: this.props.isSplit ? 0 : midHeight,
+          duration: 100
+        }
+      ).start();
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.card, styles.topCard]} >
+        <Animated.View style={[{height: this.state.height}, 
+                                styles.card, 
+                                styles.topCard]} >
           <Text style={styles.text}>
             {this.props.topText}
           </Text>
-        </View>
-        <View style={[styles.card, styles.bottomCard]} >
+        </Animated.View>
+        <View style={[ styles.card, styles.bottomCard]} >
           <Text style={styles.text}>
             {this.props.bottomText}
           </Text>
@@ -25,15 +57,18 @@ class StoryCard extends Component {
   }
 }
 
+StoryCard.propTypes = {
+  isSplit: React.PropTypes.bool.isRequired
+};
+
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
   },
   card: {
-    flex: 1,
     borderTopWidth: 1,
     borderColor: 'black',
-    padding: 30,
+    padding: 20,
   },
   topCard: {
     transform: [{rotate: '180deg'}],
