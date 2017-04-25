931aacc4782da89341a4d53cb3a027f2bd6143fa
Ako0 <amandako@gmail.com>
2017-04-24 20:31:38 -0700

    number stories (#65)
    * number stories
    
    * left align numbers

--- app/components/ChapterList.js
+++ app/components/ChapterList.js
@@ -3,6 +3,8 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  View,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import theme from '../utils/theme';
 
@@ -14,12 +16,18 @@ class ChapterList extends Component {
         automaticallyAdjustContentInsets={false}>
         {this.props.chapters.map((chapter, i) => {
           return (
-            <Text
+            <TouchableWithoutFeedback
               key={`chapter-${i}`}
-              onPress={this.props.onChapterTap.bind(null, i + 1)}
-              style={styles.item}>
-              {chapter.title}
-            </Text>
+              onPress={this.props.onChapterTap.bind(null, i + 1)}>
+              <View style={styles.row}>
+                <Text style={[styles.item, styles.index]}>
+                  {`${chapter.story}. `}
+                </Text>
+                <Text style={[styles.item, styles.title]}>
+                  {chapter.title}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
           );
         })}
       </ScrollView>
@@ -54,12 +62,22 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     backgroundColor: theme.menuBackground,
   },
+  row: {
+    flexDirection: 'row',
+  },
   item: {
     color: theme.menuText,
     fontSize: 18,
     paddingTop: 8,
     paddingBottom: 8,
     fontWeight: "500",
+  },
+  index: {
+    textAlign: 'left',
+    width: 35,
+  },
+  title: {
+    flex: 1,
   },
   rtl: {
     textAlign: 'right',
