a5a38ff3c4caef099b541957e20682aaaca09933
Ako0 <amandako@gmail.com>
2017-05-09 20:06:31 -0700

    romanian and five turkish stories (#74)

--- stories/index.js
+++ stories/index.js
@@ -2,10 +2,14 @@ import AR from './ar/chapters.js';
 import EN from './en/chapters.js';
 import FA from './fa/chapters.js';
 import SV from './sv/chapters.js';
+import RO from './ro/chapters.js';
+import TR from './tr/chapters.js';
 
 export default {
   AR,
   EN,
   FA,
   SV,
+  RO,
+  TR,
 };
