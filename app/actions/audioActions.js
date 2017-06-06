fa25c04ca79a6076d9c76b5f4b471f8148d0c79c
Brett Bukowski <brett.bukowski@gmail.com>
2017-06-05 20:22:38 -0700

    move constants

--- app/actions/audioActions.js
+++ app/actions/audioActions.js
@@ -1,4 +1,9 @@
-export const PLAY_AUDIO_TRACK = 'PLAY_AUDIO_TRACK';
+import {
+  PLAY_AUDIO_TRACK,
+  PAUSE_AUDIO_TRACK,
+  SHOW_AUDIO_MENU,
+  HIDE_AUDIO_MENU,
+} from '../constants/audioConstants';
 
 export function playAudioTrack(trackId) {
   return {
@@ -7,22 +12,17 @@ export function playAudioTrack(trackId) {
   };
 }
 
-export const PAUSE_AUDIO_TRACK = 'PAUSE_AUDIO_TRACK';
-
 export function pauseAudioTrack() {
   return {
     type: PAUSE_AUDIO_TRACK,
   };
 }
 
-export const SHOW_AUDIO_MENU = 'SHOW_AUDIO_MENU';
-
 export function showAudioMenu() {
   return {
     type: SHOW_AUDIO_MENU,
   };
 }
-export const HIDE_AUDIO_MENU = 'HIDE_AUDIO_MENU';
 
 export function hideAudioMenu() {
   return {
---
+++ app/constants/audioConstants.js
@@ -0,0 +1,6 @@
+export const S3_URL = 'https://s3-us-west-1.amazonaws.com/mission111chapteraudio';
+
+export const PLAY_AUDIO_TRACK = 'PLAY_AUDIO_TRACK';
+export const PAUSE_AUDIO_TRACK = 'PAUSE_AUDIO_TRACK';
+export const SHOW_AUDIO_MENU = 'SHOW_AUDIO_MENU';
+export const HIDE_AUDIO_MENU = 'HIDE_AUDIO_MENU';
